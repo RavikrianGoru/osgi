b86e52cc2d92cfdd8aa7c7f5688b4db559fefd7c
RavikiranGoru <ravikirangoru@gmail.com>
2018-03-20 03:33:13 +0530

    quote,quoteservice and consumer added

---
+++ com.rk.osgi.karaf.quote/src/main/java/com/rk/osgi/karaf/quote/IQuoteService.java
@@ -0,0 +1,6 @@
+package com.rk.osgi.karaf.quote;
+
+public interface IQuoteService
+{
+    String getQuote();
+}
---
+++ com.rk.osgi.karaf.quoteservice/src/main/java/com/rk/osgi/karaf/quoteservice/internal/QuoteService.java
@@ -0,0 +1,30 @@
+package com.rk.osgi.karaf.quoteservice.internal;
+
+import java.util.Random;
+
+import com.rk.osgi.karaf.quote.IQuoteService;
+
+public class QuoteService implements IQuoteService
+{
+
+    public String getQuote()
+    {
+        Random random = new Random();
+        int num = random.nextInt(3);
+        System.out.println("Generated num:"+num);
+        switch (num)
+        {
+            case 0:
+                return "service quote -0";
+            case 1:
+                return "service quote -1";
+            case 2:
+                return "service quote -2";
+            case 3:
+                return "service quote -3";
+            default:
+                return "service quote -default -100";
+        }
+    }
+
+}
